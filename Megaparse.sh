5b8e6095d25c46aa48bb196bacc6049a02439260
S. Dean Rider Jr <94093691+DeanRider@users.noreply.github.com>
2022-12-06 10:54:07 -0500

    Update Megaparse.sh

--- Megaparse.sh
+++ Megaparse.sh
@@ -3,22 +3,19 @@
 # 
 # This script will scan a folder to create a list of input mpile files 
 # It will then use each file in the list it created
-# and manipulate field 5 to separate characters.
+# and manipulate field 5 to count characters.
 # Each character represents a match or mismatch type 
 # awk will determine the counts of indels and matches; mismatch 
-# is the difference between depth and matches.
-# The data will then be printed out on a per line basis
-# use: bash Megaparse.sh
-# written July 4, 2022 by S. Dean Rider Jr. based on the following:
-# https://www.unix.com/unix-for-dummies-questions-and-answers/179555-count-different-characters-one-column.html
-# answer by scrutinizer
-# and
-# https://www.xmodulo.com/read-column-data-text-file-bash.html
+# is the difference between depth and matches + deletions.
+# The data will then be printed out on a per line basis as
+# pairs of position based xy data for scatter plots
+# use: bash Megaparse2.1.sh
+# revised December 6, 2022 by S. Dean Rider Jr.
 # 
 #######################################################################
 
 # scan directory structure for Mpile results and store as a list
-find . -name "*Mpile*" -type file -exec echo '{}' >> parselist.txt \;
+find . -name "*pileup.txt" -type file -exec echo '{}' >> parselist.txt \;
 
 echo 
 echo " ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄            ▄▄▄▄▄▄▄▄▄▄▄                           ";
@@ -51,9 +48,28 @@ echo
 while read pileref; do
 
 echo $pileref
- awk 'BEGIN{split(" [\.\,] [\+] [\-] ",S)}{for(i in S){n=gsub(S[i],x,$5); s=s (s?",":x) " " S[i]"="n}NF-=1; $5=s", []="$4 - n; s=x; }1' "$pileref" | tr -d ',[]\+\-\=\.' >> $pileref.counts.txt || true
 
-awk 'BEGIN{split(" [\.\,] [\+] [\-] ",S)}{for(i in S){n=gsub(S[i],x,$5); s=s (s?",":x) " " S[i]"="n}NF-=1; $5=s", [MM]="$4 - n; s=x; }1' "$pileref" >> $pileref.LabeledCounts.txt || true
+echo $'LOCATION\tPOSITION\tBASE\tDEPTH\tPOSITION\tMATCHES\tPOSITION\tINSERTION_EVENTS\tPOSITION\tDELETION_EVENTS\tPOSITION\tDELETIONS\tPOSITION\tMISMATCH_CALC' > $pileref.newCounts.txt
+echo $'LOCATION\tPOSITION\tBASE\tDEPTH\tPOSITION\tMATCHES\tPOSITION\tINSERTION_EVENTS\tPOSITION\tDELETION_EVENTS\tPOSITION\tDELETIONS\tPOSITION\tMISMATCH_CALC' > $pileref.perThousand.txt
+
+#  ####
+# https://unix.stackexchange.com/questions/670184/how-can-i-count-number-of-a-specific-character-in-a-column-for-each-line-and-add
+# The gsub() function returns the number of made substitutions. 
+# You may use this fact to count the number of N characters in the 2nd field and to add this number as a new field on each line:
+# example: awk -F '\t' '{ $3 = gsub("N","N",$2) }; 1' file
+# ####
+
+# current Mpileup.txt layout is \t separated
+# LOCATION POSITION BASE DEPTH PILEUP BASE-QUALITIES
+# pileup that counts as bases are .,* but not +-^$ However, ACGTacgt are problems because they can be mismatches or part of indels
+# need to confirm that depth minus matches and deleted bases equals mismatches and then
+# want to print LOCATION POSITION BASE DEPTH then calcs from gsub of the following: [\.\,] [\+] [\*] [\-] then depth minus .,*
+
+# generate table that is easy to graph in excel using pairs of columns
+awk 'BEGIN{FS=OFS="\t"} {depth=$4; insertion=gsub(/\+/,"",$5); deletion=gsub(/\*/,"",$5); delevent=gsub(/\-/,"",$5); commas=gsub(/,/,"",$5); periods=gsub(/\./,"",$5); matches=commas+periods; mismatch=depth-commas-periods-deletion} { print $1,$2,$3,$4,$2,matches,$2,insertion,$2,delevent,$2,deletion,$2,mismatch }' "$pileref" >> $pileref.newcounts.txt || true
+
+# duplicate above and make new frequencies per thousand table
+awk 'BEGIN{FS=OFS="\t"} {depth=$4; insertion=gsub(/\+/,"",$5); deletion=gsub(/\*/,"",$5); delevent=gsub(/\-/,"",$5); commas=gsub(/,/,"",$5); periods=gsub(/\./,"",$5); matches=commas+periods; mismatch=depth-commas-periods-deletion} { print $1,$2,$3,$4,$2,matches/depth*1000,$2,insertion/depth*1000,$2,delevent/depth*1000,$2,deletion/depth*1000,$2,mismatch/depth*1000 }' "$pileref" >> $pileref.perThousand.txt || true
 
 echo -e "\033[1;34m -. .-.   .-. .-.   .-. .-.   .-. .-.   .-. .-.   .-. .-.   .\033[0m";
 echo -e "\033[1;30m ||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|||\|||\ /|\033[0m";
